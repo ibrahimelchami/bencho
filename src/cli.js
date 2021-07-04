4dcf09499800d40e24fb6445d7aa7df9932e3f0d
Ibrahim El Chami <Ibrahim El Chami>
2021-07-04 04:17:26 -0700

    Copying through templates instead of strings

--- src/cli.js
+++ src/cli.js
@@ -2,10 +2,10 @@ import arg from 'arg';
 import inquirer from 'inquirer';
 import {createComponent} from "./main";
 
-function parseArgumentIntoOptions(rawArgs) {
+function parseArgumentsIntoOptions(rawArgs) {
     const args = arg(
         {
-            '--test': Boolean,
+            '--tests': Boolean,
             '--hooks': Boolean,
             '-t': '--test',
             '-h': '--hooks',
@@ -16,29 +16,96 @@ function parseArgumentIntoOptions(rawArgs) {
     );
     return{
         componentName: args._[0],
+        _test: args['--tests'] || false,
+        hooks: args['--hooks'] || false
     }
 }
 
 async function promptForMissingOptions(options) {
     const questions = [];
-
     if (!options.componentName) {
         questions.push({
             type: 'input',
             name: 'componentName',
-            message: 'Please name the component',
+            message: 'Name of your component:',
+        });
+    }
+
+    if (!options._tests) {
+        questions.push({
+            type: 'confirm',
+            name: '_test',
+            message: 'Add tests?',
+            default: false,
+        });
+    }
+
+    if (!options.hooks) {
+        questions.push({
+            type: 'confirm',
+            name: 'hooks',
+            message: 'Add hooks?',
+            default: false,
         });
     }
 
     const answers = await inquirer.prompt(questions);
-    return{
+    return {
         ...options,
-        componentName: options.componentName || answers.componentName
+        componentName: options.componentName || answers.componentName,
+        _tests: options._tests || !answers._tests,
+        hooks: options.hooks || !answers.hooks,
     };
 }
 
 export async function cli(args) {
-    let options = parseArgumentIntoOptions(args);
+    let options = parseArgumentsIntoOptions(args);
     options = await promptForMissingOptions(options);
     await createComponent(options);
 }
+
+
+// import arg from 'arg';
+// import inquirer from 'inquirer';
+// import {createComponent} from "./main";
+//
+// function parseArgumentIntoOptions(rawArgs) {
+//     const args = arg(
+//         {
+//             '--test': Boolean,
+//             '--hooks': Boolean,
+//             '-t': '--test',
+//             '-h': '--hooks',
+//         },
+//         {
+//             argv: rawArgs.slice(2),
+//         }
+//     );
+//     return{
+//         componentName: args._[0],
+//     }
+// }
+//
+// async function promptForMissingOptions(options) {
+//     const questions = [];
+//
+//     if (!options.componentName) {
+//         questions.push({
+//             type: 'input',
+//             name: 'componentName',
+//             message: 'Please name the component',
+//         });
+//     }
+//
+//     const answers = await inquirer.prompt(questions);
+//     return{
+//         ...options,
+//         componentName: options.componentName || answers.componentName
+//     };
+// }
+//
+// export async function cli(args) {
+//     let options = parseArgumentIntoOptions(args);
+//     options = await promptForMissingOptions(options);
+//     await createComponent(options);
+// }
--- src/main.js
+++ src/main.js
@@ -1,23 +1,85 @@
 import chalk from 'chalk';
 import fs from 'fs';
+import fse from 'fs-extra';
+import path from 'path';
+import { promisify } from 'util';
+
+const access = promisify(fs.access);
+
+async function copyTemplate(options) {
+    // Make component Directory
+    fs.mkdirSync(options.componentName, {recursive: true});
+
+    // Copy template + Filters
+    fse.copySync(options.templateDirectory, `${options.targetDirectory}/${options.componentName}`, {
+        overwrite: true,
+        filter: path => {
+            console.log('path ===', path)
+            return (options._tests ? !(path.indexOf('__tests__') > -1) : -1) && (options.hooks ? !(path.indexOf('hooks') > -1) : -1)
+        }
+        });
+
+    // Rename files
+    const files = fs.readdirSync(options.componentName);
+    files.forEach(file => {
+        fs.renameSync(
+            options.componentName + `/${file}`,
+            options.componentName + `/${file.replace('componentName', options.componentName)}`,
+            err => console.log(err)
+        )
+        console.log(options.componentName + `/${file.replace('componentName', options.componentName)}`)
+    });
+}
 
 export async function createComponent(options) {
     options = {
         ...options,
-        targetDirectory: options.targetDirectory || process.cwd(),
+        targetDirectory: process.cwd(),
     };
 
-    //Component name
-    const componentName = options.componentName.toLowerCase();
-    fs.mkdir(`${componentName}`,(err) => {
-        if (err) throw err;
-        console.log('Folder created!');
-    });
-    fs.appendFile(`${componentName}` + "/" + `${componentName}`+".tsx", 'import styled from "styled-components";',(err) => {
-        if (err) throw err;
-        console.log('Saved!');
-    });
+    const currentFileUrl = import.meta.url;
+    const templateDir = path.resolve(
+        new URL(currentFileUrl).pathname,
+        '../../templates',
+        'component'
+    );
+    options.templateDirectory = templateDir;
 
-    console.log('%s Project ready', chalk.blue.bold('DONE'));
+    try {
+        await access(templateDir, fs.constants.R_OK);
+    } catch (err) {
+        console.error('%s Invalid template name', chalk.red.bold('ERROR'));
+        process.exit(1);
+    }
+
+    console.log('Copy project files');
+    await copyTemplate(options);
+
+    console.log('%s Project ready', chalk.green.bold('DONE'));
     return true;
 }
+
+
+// import chalk from 'chalk';
+// import fs from 'fs';
+//
+// export async function createComponent(options) {
+//     options = {
+//         ...options,
+//         targetDirectory: options.targetDirectory || process.cwd(),
+//     };
+//
+//     //Component name
+//     const componentName = options.componentName.toLowerCase();
+//     fs.mkdir(`${componentName}`,(err) => {
+//         if (err) throw err;
+//         console.log('Folder created!');
+//     });
+//     fs.appendFile(`${componentName}` + "/" + `${componentName}`+".tsx", 'import styled from "styled-components";',(err) => {
+//         if (err) throw err;
+//         console.log('Saved!');
+//     });
+//
+//     console.log('%s Project ready', chalk.blue.bold('DONE'));
+//     return true;
+// }
